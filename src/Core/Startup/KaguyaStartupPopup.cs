5d4c96f14b71b20e61cf1390093c7d0844667da4
青白影 <142860251+qbyoffice@users.noreply.github.com>
2026-09-26 00:47:49 +0800

    Update KaguyaStartupPopup.cs

--- src/Core/Startup/KaguyaStartupPopup.cs
+++ src/Core/Startup/KaguyaStartupPopup.cs
@@ -133,13 +133,13 @@ public partial class KaguyaStartupPopup : Control, IScreenContext
             + "[b][color= red]请确认当前模式是否适用于你所处的环境。[/color][/b]\n\n"
             + "我们在选角界面为您提供了以下四个选择模式：\n"
             + "[b][color=#FFD700]Live mode[/color][/b]·[b][color=#FFD700]Live mode cat[/color][/b]·[b][color=#FFD700]Vainilla[/color][/b]·[b][color=#FFD700]Vainilla cat[/color][/b]\n"
-            + "\n[color= red]我们并未承诺过制作任何R18+模式\nVainilla模式只是不适合所有场景[/color]\n"
+            + "\n[color= red]我们并未承诺过制作任何 R-18 模式\nVainilla模式只是不适合所有场景[/color]\n"
             + "\n如果遇到问题\n请在我的[b]GitHub仓库[/b]或[b]创意工坊[/b]提交 issues。\n",
             $"Thank you for using this skin mod!\n\nCurrent mode: [b][color=#FFD700]{mode}[/color][/b]\n"
             + "[color= red]make sure your selected mode is appropriate for your environment.[/color]\n\n"
             + "Four modes are available on the character selection screen:\n"
             + "[b][color=#FFD700]Live mode[/color][/b]·[b][color=#FFD700]Live mode cat[/color][/b]·[b][color=#FFD700]Vainilla[/color][/b]·[b][color=#FFD700]Vainilla cat[/color][/b]\n"
-            + "\n[color= red]We never promised to make any R18+ mode.\nVainilla mode just isn't suitable for every situation.[/color]\n"
+            + "\n[color= red]We never promised to make any R-18 mode.\nVainilla mode just isn't suitable for every situation.[/color]\n"
             + "\nIf you encounter any issues\nreport them on my GitHub repository or Steam Workshop page.\n");
         if (error != null) body += "\n[color=#ffaaaa]" + error + "[/color]";
         _panel.SetText(Localize("来自 KaguyaSilentRavenSkin 的内容", "A message from KaguyaSilentRavenSkin"), body);
